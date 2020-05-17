3bec37403f0e05d4461f6b0198a94ad7aed405bb
Björn Andersson <ba@sanitarium.se>
2020-05-18 07:37:54 +0800

    Remove debug logging

--- chrome.js
+++ chrome.js
@@ -10,16 +10,12 @@ chrome.webRequest.onBeforeRequest.addListener(function (details) {
 
   const spotifyUri = spotifyLink.createDesktop(details.url)
   if (!spotifyUri) return
-  console.log('++++')
-  console.log(details)
-  console.log('++++')
 
   // If the current tab url is the same as the one initiating this
   // request it means this is a newly opened tab, so it is safe to
   // close the tab after Spotify has been opened.
   if (details.tabId >= 0) {
     chrome.tabs.get(details.tabId, function (tab) {
-      console.log('going in!')
       if (tab.url === details.url) {
         setTimeout(function () {
           chrome.tabs.remove(details.tabId)
