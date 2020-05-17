954133e6fda5a68d17ae15b068c38f4c76f97761
Björn Andersson <ba@sanitarium.se>
2020-05-17 11:22:58 +0800

    Format according to standardjs

--- open-in-client.js
+++ open-in-client.js
@@ -1,29 +1,30 @@
-var urls = {
+/* global chrome */
+const urls = {
   urls: [
     '*://open.spotify.com/*',
     '*://play.spotify.com/*'
   ]
-};
+}
 
-chrome.webRequest.onBeforeRequest.addListener(function(details) {
-  var domainEnds = details.url.indexOf('/', 8) + 1, // https://.length == 8
-      uriPart = details.url.slice(domainEnds).replace(/\//g, ':'),
-      isEmbedLink = details.url.indexOf('/embed/') !== -1;
+chrome.webRequest.onBeforeRequest.addListener(function (details) {
+  const domainEnds = details.url.indexOf('/', 8) + 1 // https://.length == 8
+  const uriPart = details.url.slice(domainEnds).replace(/\//g, ':')
+  const isEmbedLink = details.url.indexOf('/embed/') !== -1
 
-  if(uriPart && !isEmbedLink) {
+  if (uriPart && !isEmbedLink) {
     // If the current tab url is the same as the one initiating this
     // request it means this is a newly opened tab, so it is safe to
     // close the tab after Spotify has been opened.
-    chrome.tabs.get(details.tabId, function(tab) {
-      if(tab.url === details.url) {
-        setTimeout(function() {
-          chrome.tabs.remove(details.tabId);
-        }, 100);
+    chrome.tabs.get(details.tabId, function (tab) {
+      if (tab.url === details.url) {
+        setTimeout(function () {
+          chrome.tabs.remove(details.tabId)
+        }, 100)
       }
-    });
+    })
 
     return {
-      redirectUrl: 'spotify:'+ uriPart
-    };
+      redirectUrl: 'spotify:' + uriPart
+    }
   }
-}, urls, ['blocking']);
+}, urls, ['blocking'])
